ec139e5eb1377944dfdf32d3ca9f899ae709edba
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2026-06-26 16:49:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ cur_dir=$(pwd)
 # check os
 if [[ -f /etc/redhat-release ]]; then
     release="centos"
+elif cat /etc/issue | grep -Eqi "Armbian"; then
+    release="debian"
 elif cat /etc/issue | grep -Eqi "debian|bookworm"; then
     release="debian"
 elif cat /etc/issue | grep -Eqi "ubuntu|lunar|jammy"; then
